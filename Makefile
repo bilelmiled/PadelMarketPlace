a78129f5df17cebedad76bb482936c4c367764e4
Bilel Miled <bilel.miiled@gmail.com>
2026-03-30 12:38:19 +0100

    docker

---
+++ Makefile
@@ -0,0 +1,6 @@
+up:
+	docker-compose up --build
+
+run:
+	cd frontend && code .
+	cd backend && code .
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: true, // Écoute sur tous les adresses, y compris Docker
+    port: 5173,
+    watch: {
+       usePolling: true, // Nécessaire pour le hot-reload sous Docker Windows
+    },
+  },
 })
